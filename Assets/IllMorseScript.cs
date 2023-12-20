9a9bf0eee54ed04bf8ec773c9ccfd5066b2146a5
Crazycaleb <52386586+Crazycaleb@users.noreply.github.com>
2023-12-20 00:36:44 -0500

    Initial commit

---
+++ Assets/IllMorseScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+using KModkit;
+
+public class IllMorseScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+
+    private int _moduleId;
+    private static int _moduleIdCounter = 1;
+    private bool _moduleSolved;
+
+    String morseCombinations[] = {".", "-", ".-", "-.", "..", "--", "...", "---", ".--", "..-", "-..", "--.", "-.-", ".-.", 
+    "....", "...-", "..--", ".---", "----", "---.", "--..", "-...", "..-.", ".-..", ".--.", "-.--", "--.-", "-..-",
+    ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", }
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
